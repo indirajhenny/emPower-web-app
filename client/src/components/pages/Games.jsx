1edd97ede18003e3b4c24905953204e79b07ce70
carlito06 <carlito1006@live.com>
2021-06-17 22:57:01 -0400

    test

--- client/src/components/pages/Games.jsx
+++ client/src/components/pages/Games.jsx
@@ -2,6 +2,7 @@ import React, { Component } from 'react';
 import Button from 'react-bootstrap/Button';
 import 'bootstrap/dist/css/bootstrap.min.css';
 
+
 class Games extends Component {
   render() {
     return (
