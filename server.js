125e64a02c26dc83c21cbe9020ba9b501e5c395a
Indira Avendano <indirajhenny@Indiras-MacBook-Pro.local>
2021-11-28 06:34:17 -0500

    json change for deployment

--- server.js
+++ server.js
@@ -87,8 +87,7 @@ if (process.env.NODE_ENV == 'production') {
   app.get('*', (req,res) =>{
       res.sendFile(path.join(__dirname,'client', 'build', 'index.html'));
   });
-  //app.use('/', express.static(path.join(__dirname, '/client/build')));
-  //app.use(express.static('client/build'));
+
 }
 
 // log every single request that is coming in
